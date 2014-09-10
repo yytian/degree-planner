4d5c97d64af749104693564f12bc3333f6393a96
yytian <yytian@uwaterloo.ca>
2014-09-10 17:36:28 -0400

    finished BCS specification

--- src/degree_planner/data/scraper.clj
+++ src/degree_planner/data/scraper.clj
@@ -11,23 +11,23 @@
   (let [body (:body (client/get (str "http://www.ucalendar.uwaterloo.ca/1415/COURSE/course-" dep ".html")))]
     (map #(first (html/html-snippet %)) (next (re-seq #"<center>.*?</center>" body)))))
 
-(defn read-course [course dep]
-  { :department dep
-    :id (:name (:attrs (first (html/select course [[html/first-child :tr] :a]))))
-    :name (get-text (html/select course [[(html/nth-child 2) :tr] :b]))
-    :description (string/replace (get-text (html/select course [[(html/nth-child 3) :tr] :td])) #"\[.*\]" "")
-    :offerings (process-offerings (re-find #"Offered:.*\]" (pr-str (:content course))))
-  })
-
 (defn get-text [tag]
   (str (first (:content (first tag)))))
 
 (defn process-offerings [line]
   (apply list (filter identity (map #(case % \F "Fall" \W "Winter" \S "Spring" nil) (seq line)))))
 
+(defn read-course [course dep]
+  { :department dep
+    :id (keyword (:name (:attrs (first (html/select course [[html/first-child :tr] :a])))))
+    :name (get-text (html/select course [[(html/nth-child 2) :tr] :b]))
+    :description (string/replace (get-text (html/select course [[(html/nth-child 3) :tr] :td])) #"\[.*\]" "")
+    :offerings (process-offerings (re-find #"Offered:.*\]" (pr-str (:content course))))
+  })
+
 (defn scrape []
   (doseq [dep ["CS" "MATH" "CO" "PMATH" "STAT" "AMATH"]]
     (with-open [wrtr (io/writer (str "data/courses/" dep "-courses.edn"))]
       (.write wrtr (pr-str (map #(read-course % dep) (courses dep)))))))
 
-; (scrape)
+(scrape)
